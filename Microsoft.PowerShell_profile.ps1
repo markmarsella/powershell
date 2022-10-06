6008986f054050abbe49b869337ba2a0c1a7fd7f
Mark Marsella <mark.marsella@gmail.com>
2022-10-06 13:28:18 +0100

    Added git aliases

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,4 @@
 oh-my-posh --init --shell pwsh --config https://raw.githubusercontent.com/markmarsella/powershell/main/powershell.json?token=GHSAT0AAAAAABZTQVYFWHLZCTYIO6A7OUIAYZ6TAAQ | Invoke-Expression
+
+Import-Module git-aliases -DisableNameChecking
+New-Alias -Name c -Value clear
