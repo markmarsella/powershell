5901bc4421a83f6467eaf7940e15b7bd3ce37fe1
Mark Marsella <mark.marsella@gmail.com>
2022-10-06 13:37:19 +0100

    Use git link for config

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-oh-my-posh --init --shell pwsh --config https://raw.githubusercontent.com/markmarsella/powershell/main/powershell.json?token=GHSAT0AAAAAABZTQVYFWHLZCTYIO6A7OUIAYZ6TAAQ | Invoke-Expression
+oh-my-posh --init --shell pwsh --config https://raw.githubusercontent.com/markmarsella/powershell/main/powershell.json | Invoke-Expression
 
 Import-Module git-aliases -DisableNameChecking
 New-Alias -Name c -Value clear
